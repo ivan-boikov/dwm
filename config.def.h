556b1f3fbbdf8d60a488fcf7f45fb6a1a3817479
ivan-boikov <ik.boikov.public@protonmail.com>
2021-12-16 16:41:29 +0100

    focusonclick

--- config.def.h
+++ config.def.h
@@ -16,6 +16,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
