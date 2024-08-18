b5011920741b5d881c71ad0ba745c3325d262272
ivan-boikov <ik.boikov.public@protonmail.com>
2024-08-18 21:45:27 +0200

    XK_0 as tag 10

--- config.def.h
+++ config.def.h
@@ -116,8 +116,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -135,6 +133,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_0,                      9)
 	// layout
 	{ MODKEY,                       XK_t,      setlayout, {.v = &layouts[0] } }, // tile
 	{ MODKEY|ShiftMask,             XK_t,      setlayout, {.v = &layouts[5] } }, // bstack
